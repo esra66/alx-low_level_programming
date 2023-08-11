9b1d54e696aee547ad5ba16086480b65c99a40a1
esra66 <esra66mhmed@gmail.com>
2023-08-11 17:46:19 +0200

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *  * main - entry point
- *   *
- *    * Description: prints Hello ewrhyb
- *     *
- *      * Return: 0 
- */
+ *  *  * main - entry point
+ *   *   *
+ *    *    * Description: prints Hello ewrhyb
+ *     *     *
+ *      *      * Return: 0
+ *       */
 
-int main(void) 
-        
+int main(void)
+
 {
-puts(""Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
