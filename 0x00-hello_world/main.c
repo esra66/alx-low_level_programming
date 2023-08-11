0ba70cb9288feace42a20732ab175b69f8d48dc9
esra66 <esra66mhmed@gmail.com>
2023-08-11 14:55:28 +0200

    a

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - entry point
+ *   *
+ *    * Description: prints Hello ewrhyb
+ *     *
+ *      * Return: 0
+ *       */
+int main(void)
+{
+	    printf("Hello");
+
+	        return (0);
+}
