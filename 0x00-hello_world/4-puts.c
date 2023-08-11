170631c195e0d5810ef4a65a2f333eab5b211583
esra66 <esra66mhmed@gmail.com>
2023-08-11 17:39:56 +0200

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,12 @@
  *   *
  *    * Description: prints Hello ewrhyb
  *     *
- *      * Return: 0
- *       */
+ *      * Return: 0 
+ */
 
- int main(void) 
+int main(void) 
         
 {
-       	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+puts(""Programming is like building a multilingual puzzle\n");
+return (0);
 }
