e1ecd049a7601faf08bb1d4a28c78b7ec8c08d09
esra66 <esra66mhmed@gmail.com>
2023-08-13 21:25:45 +0200

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ *  * main - Entry point
+ *   * 
+ *    * Return: Always 0
+ *     */
+int main(void)
+{
+int x;
+for (x = 'z'; x >= 'a' ; x--)
+putchar(x);
+putchar('\n');
+return (0);
+}
