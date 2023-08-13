d4f6246468d730418dabec7a29f542d1fa3042ee
esra66 <esra66mhmed@gmail.com>
2023-08-13 20:54:27 +0200

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *\
+ int main(void)
+ {
+ int i;
+ for (i=0 ; i<10 ; i++)
+ printf("%d" , i);
+ printf("\n");
+ return(0);
+ }
