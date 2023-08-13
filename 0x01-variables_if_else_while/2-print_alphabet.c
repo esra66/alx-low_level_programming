a7a2755ad0bc9a36628cf64a888e5863b6c2397b
esra66 <esra66mhmed@gmail.com>
2023-08-13 15:35:22 +0200

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,10 +11,9 @@
  */
 int main(void)
 {
-for (int x = 'A'; x <= 'z'; x++) 
-{
-x = tolower(x);
-putchar(x);
-}
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+putchar('\n');
 return (0);
 }
