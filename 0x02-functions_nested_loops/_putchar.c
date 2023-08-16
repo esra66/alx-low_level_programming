87a771dfef065dae3e01c063ea3ebebfed866768
esra66 <esra66mhmed@gmail.com>
2023-08-16 16:07:35 +0200

    a

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include<unistd.h>
+#include "main.h"
+/**
+ *  * * main - entry poiny
+ *   * * Description: Display putchar ,
+ *    * * Return: Always 0 (successful)
+ */
+int main(void)
+{
+char chaaar[10] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(chaaar[i]);
+}
+_putchar(10);
+return (0);
+}
