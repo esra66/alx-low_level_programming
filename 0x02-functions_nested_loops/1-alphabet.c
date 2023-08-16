159c63511d9d3653143f92f9a75dd0a9430b991b
esra66 <esra66mhmed@gmail.com>
2023-08-16 17:45:23 +0200

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ */
+void print_alphabet(void)
+{
+int char;
+for (char = 'a'; char = 'z'; char++)
+_putchar(char);
+_putchar('\n');
+return (0);
+}
