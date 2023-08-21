387affd555fb6965c00a464892aedc1395824125
esra66 <esra66mhmed@gmail.com>
2023-08-21 22:08:58 +0200

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  * main - entry point
+ *   *
+ *    * Return: Always 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *p = &n;
+	*p = 402;
+	printf("%d\n',*p);
+}
+return(0);
