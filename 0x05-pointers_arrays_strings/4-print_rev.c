e3ae9b4dd74edc5746b13c038890aef11240c594
esra66 <esra66mhmed@gmail.com>
2023-08-22 04:44:51 +0200

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * print_rev - This function prints a reverse string
+ *   *
+ *    * @s: This is the input string
+ */
+void print_rev(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+		;
+	for (size = size - 1; s[size] != '\0'; size--)
+	{
+		_putchar([size]);
+	}
+	_putchar('\n');
+}
