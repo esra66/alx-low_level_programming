e1eec98549a630c84472628e8b4880363350c524
esra66 <esra66mhmed@gmail.com>
2023-08-26 20:01:41 +0200

    a

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	for ( i = 1; i <= 101; i++):
+		if i % 3 == 0 and i % 5 == 0:
+		print("FizzBuzz", end=" ")
+		elif i % 3 == 0:
+		print("Fizz", end=" ")
+		elif i % 5 == 0:
+		print("Buzz", end=" ")
+		else:
+		print(i, end=" ")
+		print();
+}
+return(0);
