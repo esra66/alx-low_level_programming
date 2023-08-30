bd6cdf106e73caf543bb09e293dc9ac5929117f4
esra66 <esra66mhmed@gmail.com>
2023-08-30 22:38:38 +0200

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,10 @@
-nclude "main.h"
+#include "main.h"
 /**
  *  * prim - searches for if a number is prime
  *   * @a: the number to check
  *    * @b: the numbers we'll go through
  *     * Return: Whether or not the number is a prime number
- *      */
+ */
 int prim(int a, int b)
 {
 	if (a <= 1 || a % b == 0)
@@ -19,7 +19,7 @@ int prim(int a, int b)
  *  * is_prime_number - tells us if an integer is a prime number or not
  *   * @n: the number to check
  *    * Return: 0 if the number is not prime, and 1 if it is
- *     */
+ */
 int is_prime_number(int n)
 {
 	return (prim(n, 2));
