b758bf644200395411d71443514951d0e0ee964d
esra66 <esra66mhmed@gmail.com>
2023-09-03 13:18:05 +0200

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
  *       * Return: If one of the numbers contains symbols.
  *        *         Otherwise - 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num, digit, sum = 0;
 
