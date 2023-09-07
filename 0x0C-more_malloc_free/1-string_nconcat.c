09e0d0c508466aee12ad70467de729aa788a4d13
esra66 <esra66mhmed@gmail.com>
2023-09-07 20:00:06 +0200

    function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,21 +2,53 @@
 #include<stdlib.h>
 #include"main.h"
 
-/*** *string_nconcat - entry point,
-* @s1: string to append to
-* @s2: string to concatenate from
-* @n: number of bytes from s2 to concatenate to s1
-*Return: pointer shall point to a newly allocated space in memory
-*/
+/**
+ * *string_nconcat - entry point,
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *Return: pointer shall point to a newly allocated space in memory
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *ar;
+	unsigned int i, size1, size2;
 
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	if (s2 == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{}
+	size1 = i;
+	for (i = 0; s2[i] != '\0'; i++)
+		;
+	size2 = i;
+	if (n >= size2)
+	{
+		n = size2;
+	}
 
+	ar = malloc(sizeof(char) * (size1 + n + 1));
 
-
-
-
-
-
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size1; i++)
+	{
+		ar[i] = s1[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		ar[size1] = s2[i];
+		size1++;
+	}
+	ar[size1] = '\0';
+return (ar);
 }
