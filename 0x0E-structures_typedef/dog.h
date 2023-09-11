fc23e3192a35d618d5c73ec7000a9ae6976e8a8f
esra66 <esra66mhmed@gmail.com>
2023-09-11 21:43:30 +0200

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+#include<stdio.h>
+/**
+ * struct dog - dog attribute
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * Return: Always 0.
+ * Description: The owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
+
