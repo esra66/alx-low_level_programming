a310ee38768124bc0226fa9359c129272b945e10
esra66 <esra66mhmed@gmail.com>
2023-09-12 13:59:50 +0200

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,14 +22,13 @@ int _strlen(char *s)
 }
 
 /**
- *  * *_strcpy - copies the string pointed to by src
- *   * including the terminating null byte (\0)
- *    * to the buffer pointed to by dest
- *     * @dest: pointer to the buffer in which we copy the string
- *      * @src: string to be copied
- *       *
- *        * Return: the pointer to dest
- *         */
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ * Return: the pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
@@ -51,13 +50,12 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- *  * new_dog - creates a new dog
- *   * @name: name of the dog
- *    * @age: age of the dog
- *     * @owner: owner of the dog
- *      *
- *       * Return: pointer to the new dog (Success), NULL otherwise
- *        */
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
