a6fea5e549f07328fd572197f9d944f7ffbe0cf3
esra66 <esra66mhmed@gmail.com>
2023-09-22 23:36:21 +0200

    function that print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include"lists.h"
 /**
- * first - print sentence
+ *  * mainn - prints a string before the main function is executed
  */
-
-void first(void)
+__attribute__ ((constructor))
+void mainn(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
