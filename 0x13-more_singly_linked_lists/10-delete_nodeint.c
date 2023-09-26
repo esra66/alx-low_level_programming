3284e7580424607482737aa47cc17225e30855fd
esra66 <esra66mhmed@gmail.com>
2023-09-26 16:20:26 +0200

    Delete node at certain position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include"lists.h"
+
+/**
+ * delete_nodeint_at_index - entry point
+ * @head: pointer
+ * @index: Address of node
+ * Return: 1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *ptr;
+	listint_t *tmp;
+
+	if (head == NULL)
+		return (-1);
+
+	ptr = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(ptr);
+		return (1);
+	}
+
+	while (ptr != NULL)
+	{
+		if (i == index - 1)
+			tmp = ptr;
+
+		if (i == index)
+		{
+			tmp->next = ptr->next;
+			free(ptr);
+			ptr = tmp->next;
+			return (1);
+		}
+		ptr = ptr->next;
+		i++;
+	}
+	return (-1);
+}
