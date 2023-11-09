679fca20a281088b6b0b0c167f61cff56010d9bc
esra66 <esra66mhmed@gmail.com>
2023-11-10 00:33:09 +0200

    Result

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,49 +1,51 @@
 #include "lists.h"
 
 /**
- *  * delete_dnodeint_at_index - deletes the node at index
- *     * @head: head
- *      * @index: index
- *       * Return: 1 or -1
- *        */
+ *  * delete_dnodeint_at_index - deletes the node at index of a
+ *   * dlistint_t linked list
+ *    *
+ *     * @head: head of the list
+ *      * @index: index of the new node
+ *       * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *he1;
-	dlistint_t *he2;
-	unsigned int x;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	he1 = *head;
+	h1 = *head;
 
-	if (he1 != NULL)
-		while (he1->prev != NULL)
-			he1 = he1->prev;
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
-	x = 0;
+	i = 0;
 
-	while (he1 != NULL)
+	while (h1 != NULL)
 	{
-		if (x == index)
+		if (i == index)
 		{
-			if (x == 0)
+			if (i == 0)
 			{
-				*head = he1->next;
+				*head = h1->next;
 				if (*head != NULL)
 					(*head)->prev = NULL;
 			}
 			else
 			{
-				he2->next = he1->next;
+				h2->next = h1->next;
 
-				if (he1->next != NULL)
-					he1->next->prev = h2;
+				if (h1->next != NULL)
+					h1->next->prev = h2;
 			}
 
-			free(he1);
+			free(h1);
 			return (1);
 		}
-		he2 = he1;
-		he1 = he1->next;
-		x++;
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
 
 	return (-1);
